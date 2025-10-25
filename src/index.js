a1e1f498f0558ab5df00d12c5ea1ca2eaf805639
SV-ICE <hristyuk.sv@gmail.com>
2025-10-25 19:21:21 +0300

    ✅feat: добавить перенаправление 404 и basename
    Добавлен файл `404.html` для перенаправления ненайденных страниц.
    Установлен `basename` для `BrowserRouter` в значение `/whisker`. Это необходимо для правильной работы приложения, развернутого в подкаталоге.

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <BrowserRouter basename='/'>
+    <BrowserRouter basename='/whisker'>
         <Provider store={store}>
             <App />
         </Provider>
