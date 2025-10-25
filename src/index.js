d1cbd813f4ceba25831db6f3bc980fe366898dc4
SV-ICE <hristyuk.sv@gmail.com>
2025-10-25 19:32:32 +0300

    🧩 refactor: заменить BrowserRouter на HashRouter
    Заменена BrowserRouter на HashRouter для корректной работы приложения после деплоя на gh-pages. BrowserRouter требует настройки сервера для корректной обработки URL, в то время как HashRouter работает на стороне клиента и не требует дополнительных настроек сервера.

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import store from './redux/redux-store';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from 'react-redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <BrowserRouter basename='/whisker'>
+    <HashRouter>
         <Provider store={store}>
             <App />
         </Provider>
-    </BrowserRouter>
+    </HashRouter>
 );
